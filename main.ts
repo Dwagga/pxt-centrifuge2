e07f9f34ed4ff67537533cfa22c8ba63cf057859
Dwagga <43897652+Dwagga@users.noreply.github.com>
2020-08-03 23:44:39 +0200

    changes4

--- main.ts
+++ main.ts
@@ -1,19 +1,9 @@
-/**
- * Write a value to the servo to control the rotation of the shaft. On a standard servo, this will
- * set the angle of the shaft (in degrees), moving the shaft to that orientation. On a continuous
- * rotation servo, this will set the speed of the servo (with ``0`` being full-speed in one
- * direction, ``180`` being full speed in the other, and a value near ``90`` being no movement).
- * @param name pin to write to
- * @param value angle or rotation speed
- */
-//% help=pins/servo-write weight=41 group="Servo"
-//% blockId=device_set_servo_pin block="servo hello hello write|pin %name|to %value=protractorPicker" blockGap=8
-//% parts=microservo trackArgs=0
-//% blockNamespace=pins
-//% name.fieldEditor="gridpicker"
-//% name.fieldOptions.width=220
-//% name.fieldOptions.columns=4
-//% value.defl=90
-void servoWrite(PwmOnlyPin name, int value) {
-    PINOP(setServoValue(value));
+namespace fun {
+    /**
+    * Computes the famous Fibonacci number sequence!
+    */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value - 1) + fib(value - 2);
+    }
 }
