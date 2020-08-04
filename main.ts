a8c85cb03bd3e866bd3f76e9b3e5d4b22753fe3d
Dwagga <43897652+Dwagga@users.noreply.github.com>
2020-08-04 09:41:13 +0200

    changes 5

--- main.ts
+++ main.ts
@@ -1,9 +1,1 @@
-namespace fun {
-    /**
-    * Computes the famous Fibonacci number sequence!
-    */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value - 1) + fib(value - 2);
-    }
-}
+// leave empty 
