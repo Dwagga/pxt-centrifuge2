b3418df694c7b764cb16f6e324329a9a37e666ca
Dwagga <43897652+Dwagga@users.noreply.github.com>
2020-08-04 09:54:11 +0200

    Adding centrifuge.ts

---
+++ centrifuge.ts
@@ -0,0 +1,20 @@
+/**
+ * Write a value to the servo to control the rotation of the shaft. On a standard servo, this will
+ * set the angle of the shaft (in degrees), moving the shaft to that orientation. On a continuous
+ * rotation servo, this will set the speed of the servo (with ``0`` being full-speed in one
+ * direction, ``180`` being full speed in the other, and a value near ``90`` being no movement).
+ * @param name pin to write to
+ * @param value angle or rotation speed
+ */
+//% block
+//% help=pins/servo-write weight=41 group="Servo"
+//% blockId=device_set_servo_pin block="servo write|pin %name|to %value=protractorPicker" blockGap=8
+//% parts=microservo trackArgs=0
+//% blockNamespace=pins
+//% name.fieldEditor="gridpicker"
+//% name.fieldOptions.width=220
+//% name.fieldOptions.columns=4
+//% value.defl=90
+void servoWrite(PwmOnlyPin name, int value) {
+    PINOP(setServoValue(value));
+}
