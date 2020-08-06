c7aa8c7f0467430a41ade058064c0c3f676fa006
Dwagga <43897652+Dwagga@users.noreply.github.com>
2020-08-06 20:14:54 +0200

    Update centrifuge.ts

--- centrifuge.ts
+++ centrifuge.ts
@@ -14,14 +14,14 @@ namespace centrifuge {
     }
     
     //% help=pins/servo-write-pin weight=20
-    //% blockId=device_set_servo_pin block="servo write|pin %time|to %value" blockGap=8
+    //% blockId=device_set_servo_pin block="servo write|pin %name|to %value" blockGap=8
     //% parts=microservo trackArgs=0
     //% value.min=0 value.max=180
-    //% time.fieldEditor="gridpicker" time.fieldOptions.columns=6
-    //% time.fieldOptions.tooltips="false" time.fieldOptions.width="250"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=6
+    //% name.fieldOptions.tooltips="false" name.fieldOptions.width="250"
     //% weight=100 color=#0fbc11
-    export function servo(time:number ,value: number): void {
-        pins.D13.servoWrite(value)
+    export function servo(name:number ,value: number): void {
+        pins.servoWrite(name, value)
     }
 }
     
