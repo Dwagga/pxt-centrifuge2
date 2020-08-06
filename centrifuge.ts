bbb5a1bdd47151561460615d483df84ee829c87e
Dwagga <43897652+Dwagga@users.noreply.github.com>
2020-08-06 20:07:45 +0200

    Update centrifuge.ts

--- centrifuge.ts
+++ centrifuge.ts
@@ -1,10 +1,3 @@
-/**
- * Functions are mapped to blocks using various macros
- * in comments starting with %. The most important macro
- * is "block", and it specifies that a block should be
- * generated for an **exported** function.
- */
-
 //% color="#AA278D" weight=100
 namespace centrifuge {
     //% block
@@ -21,14 +14,14 @@ namespace centrifuge {
     }
     
     //% help=pins/servo-write-pin weight=20
-    //% blockId=device_set_servo_pin block="servo write|pin %name|to %value" blockGap=8
+    //% blockId=device_set_servo_pin block="servo write|pin %time|to %value" blockGap=8
     //% parts=microservo trackArgs=0
     //% value.min=0 value.max=180
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=6
-    //% name.fieldOptions.tooltips="false" name.fieldOptions.width="250"
+    //% time.fieldEditor="gridpicker" time.fieldOptions.columns=6
+    //% time.fieldOptions.tooltips="false" time.fieldOptions.width="250"
     //% weight=100 color=#0fbc11
-    export function servo(name:number ,value: number): void {
-        pins.servoWritePin(name, value)
+    export function servo(time:number ,value: number): void {
+        pins.D13.servoWrite(value)
     }
 }
     
